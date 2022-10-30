68e8a05b14c256c809cfa38ffff4a5a5686d8dce
Mayank <kaushalmayank.k@gmail.com>
2022-10-30 23:13:41 +0530

    Welcome page links added

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -47,7 +47,7 @@ export const Form = () => {
     <div id = "page" >
 
     <form method="POST" id ="form-box">
-        <img src="https://media.istockphoto.com/vectors/register-account-submit-access-login-password-username-internet-vector-id1281150061?k=20&m=1281150061&s=612x612&w=0&h=wpCvmggedXRECWK-FVL98MMllubyevIrXuUu50fdCqE=" alt="Register" />
+        <img id='signupImage' src="https://media.istockphoto.com/vectors/register-account-submit-access-login-password-username-internet-vector-id1281150061?k=20&m=1281150061&s=612x612&w=0&h=wpCvmggedXRECWK-FVL98MMllubyevIrXuUu50fdCqE=" alt="Register" />
         
         <div id = "form">
         <h1 style={{marginBottom:"30px", fontWeight:'bold'}}>Sign Up</h1>
--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './Welcome.css'
+
 const Welcome = (props) => {
   return (
     <>
@@ -12,7 +12,11 @@ const Welcome = (props) => {
       </div>
       <div id="whatsNew">
         <div className="carasoul">
-          <Link href=""><img src="" alt="" /></Link>
+          <Link className='carasoulLink' to="/login"><img src="https://media.istockphoto.com/id/1281150061/vector/register-account-submit-access-login-password-username-internet-online-website-concept.jpg?s=612x612&w=0&k=20&c=9HWSuA9IaU4o-CK6fALBS5eaO1ubnsM08EOYwgbwGBo=" alt="" /><p className='LinkOverText'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, tenetur veritatis ex autem perspiciatis voluptatem, assumenda nihil</p></Link>
+          <Link className='carasoulLink' to="/login"><img src="https://media.istockphoto.com/id/1281150061/vector/register-account-submit-access-login-password-username-internet-online-website-concept.jpg?s=612x612&w=0&k=20&c=9HWSuA9IaU4o-CK6fALBS5eaO1ubnsM08EOYwgbwGBo=" alt="" /><p className='LinkOverText'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, tenetur veritatis ex autem perspiciatis voluptatem, assumenda nihil</p></Link>
+          <Link className='carasoulLink' to="/login"><img src="https://media.istockphoto.com/id/1281150061/vector/register-account-submit-access-login-password-username-internet-online-website-concept.jpg?s=612x612&w=0&k=20&c=9HWSuA9IaU4o-CK6fALBS5eaO1ubnsM08EOYwgbwGBo=" alt="" /><p className='LinkOverText'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, tenetur veritatis ex autem perspiciatis voluptatem, assumenda nihil</p></Link>
+          <Link className='carasoulLink' to="/login"><img src="https://media.istockphoto.com/id/1281150061/vector/register-account-submit-access-login-password-username-internet-online-website-concept.jpg?s=612x612&w=0&k=20&c=9HWSuA9IaU4o-CK6fALBS5eaO1ubnsM08EOYwgbwGBo=" alt="" /><p className='LinkOverText'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi, tenetur veritatis ex autem perspiciatis voluptatem, assumenda nihil</p></Link>
+          
         </div>
       </div>
     </>
