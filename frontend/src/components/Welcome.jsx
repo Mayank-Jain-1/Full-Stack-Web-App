f3c98d28c7106866531c4bbcec99d5bc0262e570
Mayank <kaushalmayank.k@gmail.com>
2022-11-11 01:51:07 +0530

    welcome page improved

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Welcome.css'
 const Welcome = (props) => {
   return (
     <>
       <div className='Welcome'>
-      <h1 className='welcomeText'><span style = {{color : '#69c29f'}}>Welcome,</span>Your Account </h1>
-      <h1>has been created</h1>
-      <h1>{props.name}</h1>
+        <h1 style={{color: "white", fontSize: "10rem"}}>Welcome</h1>
+        <h1 className='welcomeText'>Your Account has</h1>
+        <h1>been created</h1>
+        <h1>{props.name}</h1>
+      </div>
+      <div id="whatsNew">
+        <div className="carasoul">
+          <Link href=""><img src="" alt="" /></Link>
+        </div>
       </div>
     </>
   )
